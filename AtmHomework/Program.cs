af082115e615a3e495e5d7c14a6e4d1f268ddaf9
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-07-30 18:25:10 +0300

    Add project files.

---
+++ AtmHomework/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace AtmHomework
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Bank bank = new Bank();
+            ATM atm1 = new ATM(bank);
+            ATM atm2 = new ATM(bank);
+
+            new Thread(() => atm1.WithdrawMoney(7000)).Start();
+            new Thread(() => atm2.WithdrawMoney(2000)).Start();
+        }
+    }
+    class Bank
+    {
+        private int accountBalance = 20000;
+        public void WithdrawMoney(int amount)
+        {
+            if (amount > accountBalance)
+            {
+                Console.WriteLine("No money on your account");
+            }
+            else
+            {
+                Thread.Sleep(1000);
+                accountBalance -= amount;
+                Console.WriteLine($"Taken - {amount}. Balance - {accountBalance}");
+            }
+        }
+    }
+    class ATM
+    {
+        private Bank bank;
+
+        public ATM(Bank bank)
+        {
+            this.bank = bank;
+        }
+        public void WithdrawMoney(int amount)
+        {
+            bank.WithdrawMoney(amount);
+        }
+    }
+}
